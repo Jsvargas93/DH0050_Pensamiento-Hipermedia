59f06e6ba2d6b5ab5de6bf510515e26ff398eb98
Jsvargas93 <126963441+Jsvargas93@users.noreply.github.com>
2023-03-04 10:39:07 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,30 +1,22 @@
 function setup() {
-  let d = 70;
-  let p1 = d;
-  let p2 = p1 + d;
-  let p3 = p2 + d;
-  let p4 = p3 + d;
-
-  // Define el lienzo de 720 pixeles de ancho y 400 de alto
   createCanvas(720, 400);
-  background(0);
-  noSmooth();
-
-  translate(140, 0);
-
-  // Dibuja una caja gris
-  stroke(153);
-  line(p3, p3, p2, p3);
-  line(p2, p3, p2, p2);
-  line(p2, p2, p3, p2);
-  line(p3, p2, p3, p3);
-
-  // Dibuja puntos blancos
   stroke(255);
-  point(p1, p1);
-  point(p1, p3);
-  point(p2, p4);
-  point(p3, p1);
-  point(p4, p2);
-  point(p4, p4);
+  noFill();
 }
+
+function draw() {
+  background(0);
+  for (let i = 0; i < 200; i += 20) {
+    bezier(
+      mouseX - i / 2.0,
+      40 + i,
+      410,
+      20,
+      440,
+      300,
+      240 - i / 16.0,
+      300 + i / 8.0
+    );
+  }
+}
+
