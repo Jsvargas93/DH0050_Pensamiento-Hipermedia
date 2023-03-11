ee53e451225266d219ecabd0b1fb289b32962baf
Jsvargas93 <126963441+Jsvargas93@users.noreply.github.com>
2023-03-11 10:47:35 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,22 +1,53 @@
+// Where is the car
+let x, y;
+
+//crearclasecarro
+class carro {
+ constructor(alto,ancho,color,name){
+  this.name=name;
+  this.alto=alto;
+  this.ancho=ancho;
+  this.color=color;
+ }
+}
+
 function setup() {
-  createCanvas(720, 400);
-  stroke(255);
-  noFill();
+  
+  createCanvas(400, 200);
+  // Starts position
+  y = height - height/4;
+  x = 0;
+
+// crearuna instancia de carro llamada mimaquina
+ let mycar = new carro (5,15,(blue),"my first carr");
 }
 
 function draw() {
-  background(0);
-  for (let i = 0; i < 200; i += 20) {
-    bezier(
-      mouseX - i / 2.0,
-      40 + i,
-      410,
-      20,
-      440,
-      300,
-      240 - i / 16.0,
-      300 + i / 8.0
-    );
+  background(200);  
+  display();
+  move();
+}
+
+
+   
+function move(){
+  // Jiggling randomly
+  y = y + random(-1, 1);
+  
+  if (x > width) {
+    // Reset 
+    x = 0;
+  } else {
+    // Moving up at a constant speed
+    x = x + 1;
   }
 }
+
+function display(){
+  // Draw a car
+  stroke(50);
+  fill(255,4,150);
+  rect(x, y, 20, 10);
+}
+
 
